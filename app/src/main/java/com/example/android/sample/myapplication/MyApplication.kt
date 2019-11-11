e49a1f31fe120445b1f737faaa88903b37eb90e9
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2019-11-11 15:12:28 +0900

    Initial commit

---
+++ app/src/main/java/com/example/android/sample/myapplication/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.example.android.sample.myapplication
+
+import android.app.Application
+import io.realm.Realm
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+
+    }
+}
