2d43eec3cf23c3c65a09619feac4a3ab18265847
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2019-11-12 15:04:08 +0900

    スマホかタブレットか判定する 変数と条件を設定

--- app/src/main/java/com/example/android/sample/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/android/sample/myapplication/MainActivity.kt
@@ -6,13 +6,22 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class MainActivity : AppCompatActivity() {
+
+    var isTwoPane: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setSupportActionBar(toolbar)
+
+        //スマホかアブレットか判定
+        //container_detailはタブレットの時だけ使うため、
+        //null出ない時は、isTwoPaneをtrueにする。
+        if (container_detail != null) isTwoPane = true
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -23,6 +32,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        menu.apply {
+            findItem(R.id.menu_delete).isVisible = false
+            findItem(R.id.menu_edit).isVisible = false
+            findItem(R.id.menu_register).isVisible = false
+        }
         return true
     }
 
