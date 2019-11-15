8878077e1bc0653cc7f7d5444ca6ed4f0b26f60d
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2019-11-15 12:49:21 +0900

    onCreateDialogの追加

--- app/src/main/java/com/example/android/sample/myapplication/DataPickerDialogFragment.kt
+++ app/src/main/java/com/example/android/sample/myapplication/DataPickerDialogFragment.kt
@@ -1,4 +1,25 @@
 package com.example.android.sample.myapplication
 
-class DataPickerDialogFragment {
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class DataPickerDialogFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val calendar = Calendar.getInstance()
+        val year = Calendar.YEAR
+        val month = Calendar.MONTH
+        val day = Calendar.DAY_OF_MONTH
+
+        return DatePickerDialog(activity, this, year, month, day)
+    }
+
+    override fun onDateSet(datePicker: DatePicker?, year: Int, month: Int, day: Int) {
+
+    }
 }
